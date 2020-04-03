6fab99ab75060adfeb52c8e84edce6c40a2aa316
Toni  Diep <adiep123@gmail.com>
2020-04-03 23:39:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,16 +4,5 @@ def prime(array)
   p = 2
   if n < 2
     return p
-    
-
-
-
-
-(2..(num - 1)).each do |number|
-    return false if number == 0
-  else
-    true
-    end
   end
-# prime number returns true
-#non-prime number returns false
+end 
