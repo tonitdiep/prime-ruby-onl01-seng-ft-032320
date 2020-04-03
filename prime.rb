d898f601bc6a28fc54248a9c4cbed4f52060de2c
Toni  Diep <adiep123@gmail.com>
2020-04-03 23:25:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 def prime(number)
 (2..(num - 1)).each do |number|
     return false if number == 0
-    end
+  else
     true
     end
+  end
 # prime number returns true
 #non-prime number returns false
