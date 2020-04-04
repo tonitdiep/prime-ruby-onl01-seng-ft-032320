109c903f415b15623ae8d582ad2665f588ae0004
Toni  Diep <adiep123@gmail.com>
2020-04-04 00:57:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(num)
   if num <= 1
     return false
-  else
-    
- (2..num-1).each do |num|
-    return true if num > -1
-end
+#  else
+
+# (2..num-1).each do |num|
+#    return true if num > -1
+#end
 end
